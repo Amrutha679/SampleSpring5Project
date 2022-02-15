18a03d1ca1c81bd1039b889b2644600a3dbad785
Amrutha <amruthaedu665@gmail.com>
2022-02-15 22:19:59 +0530

    spring loose coupling

--- src/main/java/com/travel/MainLogic.java
+++ src/main/java/com/travel/MainLogic.java
@@ -8,10 +8,11 @@ public class MainLogic {
 	public static void main(String args[]) {
 		
 		ApplicationContext ac = new ClassPathXmlApplicationContext("spring.xml");
-		Car car = (Car) ac.getBean("kia");
-		car.move();
-		Bike bike = (Bike) ac.getBean("ktm");
-		bike.move();
+		
+		Travel travel = (Travel) ac.getBean("Banglore");
+		travel.startJourney();
+		Travel travel2 = (Travel) ac.getBean("Hyderabad");
+		travel2.startJourney();
 		//System.out.println(st.getSid()+" "+st.getSname()+" "+st.getSmarks());
 	}
 }
